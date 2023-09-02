699b321ff3711495744ffb166353b223e050b836
VipulDG <142492581+VipulDG@users.noreply.github.com>
2023-09-02 14:47:02 +0530

    Add files via upload

---
+++ 21.js
@@ -0,0 +1,112 @@
+var card=document.getElementById("cards")
+var total=document.getElementById("total")
+var message=document.getElementById("message")
+var ctotal=document.getElementById("ctotal")
+var pcard=[]
+var sum1=0
+var sum=0
+var win=false
+var game=true
+var sgame=false 
+var ctotal=document.getElementById("ctotal")
+var player=prompt("enter player name")
+var amount=prompt("enter the amount")
+
+function getrandom(){
+  var random=Math.floor(Math.random()*13+1)
+  if(random>10){
+    return 10
+  }
+   else if(random==1){
+    return 11
+   }
+   else{
+    return random
+   }
+}
+
+function start() {
+     sgame=true
+    var num1=getrandom()
+    var num2=getrandom()
+    var num3=getrandom()
+    var num4=getrandom()
+      sum=num1+num2
+      sum1=sum3+sum4
+      card.innerHTML="cards: "
+      pcard=[num1,num2]
+     render()
+}
+  function render(){  
+       card.innerHTML="cards: "
+
+       for(var i=0;i<pcard.length;i++){
+       card.innerHTML += `${pcard[i]}`
+      }
+    
+      total.innerHTML = `total: ${sum}`
+      ctotal.innerHTML=`Computer total: ${sum1}`
+
+      if(sum<21){
+        message.innerHTML="do you want another card"
+      }
+      else if(sum==21){
+        amount *=2
+        message.innerHTML= `congrats${player},you won Rs.${amount}`
+        win=true
+      }
+      else if(sum==21 && sum<21){
+        amount *=2
+        message.innerHTML= `congrats${player},you won Rs.${amount}`
+        win=true
+      }
+
+      else{
+        message.innerHTML=`sorry,${player},your Rs.${amount} is gaya`
+        game=false
+      }
+    }
+
+
+ function newcard(){
+    if(win==false && game==true && sgame==true){   
+     var num=getrandom()
+     pcard.push(num)
+     sum +=num
+
+     while(sum1<19){
+      var cnum=getrandom()
+      sum1 +=cnum
+
+     }
+
+     render()
+     winner()
+    }
+    
+   
+ }
+
+
+ function winner(){
+
+  if(sgame=true){
+
+  while(sum<17 || sum1<sum){
+    var cnum=getrandom()
+    summ1 +=cnum
+ }  
+  ctotal.innerHTML=`computer total : ${sum1}`
+  if(sum1>21 || (sum1<=21 && sum1>sum)){
+    message.innerHTML=`sorry,${player},your Rs.${amount} is gaya`
+  }
+  else if(sum==sum1){
+    message.innerHTML="it is a tie"
+  }
+  else {
+    amount *=2
+    message.innerHTML= `congrats${player},you won Rs.${amount}`
+  }
+  game=false
+ }
+}
